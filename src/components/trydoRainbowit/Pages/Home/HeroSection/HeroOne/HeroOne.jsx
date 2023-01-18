ffd4927c65ae3c7abcb78edfd6e367fecb718ed6
kalam <kalam@itcbd.com>
2023-01-18 16:21:35 +0600

    trydo-website

---
+++ src/components/trydoRainbowit/Pages/Home/HeroSection/HeroOne/HeroOne.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HeroOne.css';
+const HeroOne = () => {
+    // const heroTitle = {
+    //     title: "A DIGITAL AGENCY.",
+    // }
+    return (
+        <>
+            <h1 className='heroTitle'>A DIGITAL AGENCY</h1>
+        </>
+    );
+};
+
+export default HeroOne;
---
+++ src/components/trydoRainbowit/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeroOne from './HeroSection/HeroOne/HeroOne';
+import './Home.css';
+const Home = () => {
+    return (
+        <>
+            <HeroOne />
+        </>
+    );
+};
+
+export default Home;
